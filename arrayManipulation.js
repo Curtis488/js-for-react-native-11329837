37426923ba2445e65d0ffc4dc5cadb356064ca4b
curtis488 <csnomensah@st.ug.edu.gh>
2024-05-17 21:41:35 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,3 +9,22 @@ function processArray(arr) {
          }
          return num % 2 === 0 ? num ** 2 : num * 3;
      })};
+
+     function formatArrayStrings(stringArray,numberArray) {
+        if (!Array.isArray(stringArray)|| !Array.isArray(numberArray)) {
+            throw new Error("Both inputs must be arrays");
+        }
+        if (stringArray.length !==numberArray.length){
+            throw new Error("Both arrays must be of the same lenght");
+        }
+        return stringArray.map((str,index)=> {
+            const num = 
+            numberArray[index];
+            if (typeof str !== 'string' || typeof num !== 'number') {
+                throw new Error("String array must contain strings and number array must contain number");
+            }
+            return num % 2 === 0 ? 
+            str.toUpperCase():
+            str.toLowerCase();
+        });
+    };
